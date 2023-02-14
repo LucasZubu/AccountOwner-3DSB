3f9406b7e06b9a2d22e5470932489189299d1b82
José Antonio Gallo Junior <gallojunior@gmail.com>
2023-02-14 20:27:35 -0300

    Criada a classe estática de configuração de serviços e inclusão no Program.cs

---
+++ backend/AccountOwnerServer/Extensions/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+namespace AccountOwnerServer.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy",
+                builder => builder
+                .AllowAnyOrigin()  // WithOrigins("dominio")
+                .AllowAnyMethod()  // WithMethods("GET", "POST")
+                .AllowAnyHeader()  // WithHeaders("accept", "content-type")
+            );
+        });
+    }
+
+    public static void ConfigureIISIntegration(this IServiceCollection services)
+    {
+        services.Configure<IISOptions>(options =>
+        {
+
+        });
+    }
+}
