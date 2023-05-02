282d80d476d5ca7c2e26e96879a1214920bb3056
LucasZubu <lucascaires3641@gmail.com>
2023-05-02 20:37:00 -0300

    Subindo alterações

--- frontend/AccountOwnerClient/src/app/owner/owner-routing.module.ts
+++ frontend/AccountOwnerClient/src/app/owner/owner-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OwnerListComponent } from './owner-list/owner-list.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path:'list', component: OwnerListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
